39e6370ac468fc4a1126de715c1254606fa77f58
andrealencar <isabel1000>
2016-07-05 21:07:55 -0300

    pages cache

---
+++ pages_cache/middleware/PagesRequests.py
@@ -0,0 +1,33 @@
+__author__ = 'shenoisz'
+
+from django.conf import settings
+
+class GetPageRequest( object ):
+
+    #=======================================================
+    #      add host, session and url for cache util
+    #=======================================================
+    def process_request( self, request ):
+
+        settings.PAGE_CACHE_HOST = request.META['HTTP_HOST']
+        settings.PAGE_METHOD = request.method
+        settings.PAGE_URL = request.get_full_path( )
+        settings.PAGE_SESSION = ''
+        #=============================================
+        #      if user logged add your session
+        #=============================================
+        if request.user.is_authenticated( ):
+
+            settings.PAGE_SESSION = '|'+ str( request.META['HTTP_COOKIE'] )
+
+        #======================================
+        #        create key for cache
+        #======================================
+        settings.KEY_PREFIX = (
+            str( settings.PAGE_CACHE_HOST )+
+            str( settings.PAGE_URL )+
+            str( settings.PAGE_SESSION )
+        )
+
+    def process_response( self, request, response ):
+        return response
