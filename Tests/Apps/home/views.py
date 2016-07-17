a78d5d51c6c4fbef0ecfd9f1a5be3af80027919c
SHENOISZ <marcelo.net.system@gmail.com>
2016-07-17 15:21:16 -0300

    home

---
+++ Tests/Apps/home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+# shenoisz
+from pages_cache.pages_decorators import CacheViews
+# Create your views here.
+
+register = CacheViews( )
+
+class Home( View ):
+    # add cache
+    @register.cache_view( 
+        key_prefix='home-index', 
+        expire=60 * 15 
+    )
+    def get ( self, request, *args, **kwargs ):
+
+        return render( request, 'index.html')
+
+class HomePage( View ):
+    
+    def get ( self, request, *args, **kwargs ):
+
+        return render( request, 'page.html')        
